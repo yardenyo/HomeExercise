a82783910045841f120fb366ee1513bb7d6edb33
yardenyo <gmloler@gmail.com>
2022-08-25 17:53:18 +0300

    fixed some issues

--- client/src/Pages/AddUser.js
+++ client/src/Pages/AddUser.js
@@ -11,16 +11,25 @@ function AddUser() {
     URL: "",
   });
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     axios
-      .post("/users.php", {
-        Username: data.Username,
-        Email: data.Email,
-        Password: data.Password,
-        Birthdate: data.Birthdate,
-        Phone: data.Phone,
-        URL: data.URL,
-      })
+      .post(
+        "/users.php",
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          },
+        },
+        JSON.stringify({
+          Username: data.Username,
+          Email: data.Email,
+          Password: data.Password,
+          Birthdate: data.Birthdate,
+          Phone: data.Phone,
+          URL: data.URL,
+        })
+      )
       .then((res) => {
         console.log(res);
       })
@@ -29,9 +38,9 @@ function AddUser() {
       });
   };
 
-  const handleChange = (e) => {
+  const handleChange = (event) => {
     const newData = { ...data };
-    newData[e.target.id] = e.target.value;
+    newData[event.target.id] = event.target.value;
     setData(newData);
     console.log(newData);
   };
@@ -41,56 +50,56 @@ function AddUser() {
       <form action="/" method="post">
         <label htmlFor="username">Username:</label>
         <input
-          onClick={(e) => handleChange(e)}
-          value={data.Username}
           type="text"
           id="Username"
           name="username"
+          onChange={(e) => handleChange(e)}
+          value={data.Username}
         ></input>
         <br></br>
         <label htmlFor="email">Email:</label>
         <input
-          onClick={(e) => handleChange(e)}
-          value={data.Email}
-          type="text"
+          type="email"
           id="Email"
           name="email"
+          onChange={(e) => handleChange(e)}
+          value={data.Email}
         ></input>
         <br></br>
         <label htmlFor="password">Password:</label>
         <input
-          onClick={(e) => handleChange(e)}
-          value={data.Password}
           type="text"
           id="Password"
           name="password"
+          onChange={(e) => handleChange(e)}
+          value={data.Password}
         ></input>
         <br></br>
         <label htmlFor="birthdate">Birthdate:</label>
         <input
-          onClick={(e) => handleChange(e)}
-          value={data.Birthdate}
-          type="text"
+          type="date"
           id="Birthdate"
           name="birthdate"
+          onChange={(e) => handleChange(e)}
+          value={data.Birthdate}
         ></input>
         <br></br>
         <label htmlFor="phone number">Phone number:</label>
         <input
-          onClick={(e) => handleChange(e)}
-          value={data.Phone}
-          type="number"
+          type="text"
           id="Phone"
           name="phonenumber"
+          onChange={(e) => handleChange(e)}
+          value={data.Phone}
         ></input>
         <br></br>
         <label htmlFor="URL">URL:</label>
         <input
-          onClick={(e) => handleChange(e)}
-          value={data.URL}
           type="text"
           id="URL"
           name="URL"
+          onChange={(e) => handleChange(e)}
+          value={data.URL}
         ></input>
         <br></br>
         <input type="submit" value="Submit" onClick={handleSubmit}></input>
--- server/private/Functions/userFunctions.php
+++ server/private/Functions/userFunctions.php
@@ -17,10 +17,37 @@ function getAllUsers() {
 }
 
 function addUser() {
-  $username = $_POST['username'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $phone = $_POST['phone'];
+  if ( !preg_match("/^[a-zA-Z][0-9a-zA-Z_]{2,23}[0-9a-zA-Z]$/", $_POST["username"]) ) {
+    http_response_code(400);
+    die("Error: Username must contain only letters");
+  } else {
+    $username = $_POST['username'];
+  }
+  if ( !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $_POST["email"]) ) {
+    http_response_code(400);
+    die("Error: Email must be a valid email address");
+  } else {
+    $email = $_POST['email'];
+  }
+  if ( !preg_match("/^[0-9]{10}$/", $_POST["phone"]) ) {
+    http_response_code(400);
+    die("Error: Phone must be a valid phone number");
+  } else {
+    $phone = $_POST['phone'];
+  }
+  if ( !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/", $_POST["password"]) ) {
+    http_response_code(400);
+    die("Error: Password must be at least 8 characters long");
+  } else {
+    $password = $_POST['password'];
+  }
+  if ( !preg_match('%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu' , $_POST["URL"]) ) {
+    http_response_code(400);
+    die("Error: URL must be a valid URL address");
+  } else {
+    $URL = $_POST['URL'];
+  }
+
   $URL = $_POST['URL'];
   global $conn;
   $sql = "INSERT INTO `users` (`username`, `email`, `password`, `phone number`, `URL`) VALUES ('$username', '$email', '$password', '$phone', '$URL')";
